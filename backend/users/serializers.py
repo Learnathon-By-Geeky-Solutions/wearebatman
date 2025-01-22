9d9f9b946d1267edd6cd2c7f833fb6a7f3f2d2c4
TheDeadcoder <sakibbuet2k19@gmail.com>
2025-01-22 20:19:14 +0600

    [FIX and TEST] user features

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -4,11 +4,11 @@ from .models import ParentUser, ChildUser
 class ParentUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = ParentUser
-        fields = 'all'
+        fields = '__all__'
         read_only_fields = ['created_at', 'updated_at', 'id']
 
 class ChildUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = ChildUser
-        fields = 'all'
+        fields = '__all__'
         read_only_fields = ['created_at', 'updated_at', 'id']
--- backend/users/views.py
+++ backend/users/views.py
@@ -5,7 +5,13 @@ from .serializers import ParentUserSerializer, ChildUserSerializer
 class ParentUserViewSet(viewsets.ModelViewSet):
     queryset = ParentUser.objects.all()
     serializer_class = ParentUserSerializer
+    def update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return super().update(request, *args, **kwargs)
 
 class ChildUserViewSet(viewsets.ModelViewSet):
     queryset = ChildUser.objects.all()
     serializer_class = ChildUserSerializer
+    def update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return super().update(request, *args, **kwargs)
